76c06f5cd6551b4ccdacacaa520ec23baab6dd44
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2025-01-20 03:48:24 +0500

    Q2 - Updated docs

---
+++ Q2-query-optimization.sql
@@ -0,0 +1,7 @@
+/**
+    This problem was something I solved in one of my workplace bugs / stories.
+
+    Problem: 
+    The following query has a FULL table scan for People table that can be optimized
+    Explain why and how it can be optimized
+*/
